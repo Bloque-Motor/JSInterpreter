7bd03c2fccc5a66890c74c770d40a6216f7ef9de
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2018-10-21 20:32:59 +0200

    Counting characters and lines
    The code will count all the characters and separate them into several character types.
    
    The character and line count will be output at the end of the execution.

--- src/main.kt
+++ src/main.kt
@@ -4,16 +4,45 @@ fun main(args : Array<String>) {
     println("Enter filename to parse:")
     val fileName: String? = readLine()
     val file = File(fileName)
-    var fileExists = file.exists()
+    val fileExists = file.exists()
     if(fileExists){
-        println("$fileName does exist.")
+        println("$fileName does exist. Reading file...")
+        println()
         val bufferedReader = file.bufferedReader()
         val text:List<String> = bufferedReader.readLines()
+        var caps = 0
+        var small = 0
+        var other = 0
+        var numbers = 0
+        var lines = 0
         for(line in text){
             println(line)
+            var currentLine = line.toCharArray()
+            var char: Char?
+            for (char in currentLine){
+                    when{
+                        (char in 'a'..'z')-> small++
+
+                        (char in 'A'..'Z')-> caps++
+
+                        (char in '0'..'9')-> numbers++
+
+                        else-> other++
+                    }
+                }
+            lines++
         }
+        println()
+        println("___________________________________")
+        println("Chacacter count")
+        println()
+        println("Caps: $caps")
+        println("Small letters: $small")
+        println("Numbers: $numbers")
+        println("Other characters: $other")
+        println("""Total number of characters: ${small + caps + numbers + other}""")
+        println("Number of lines: $lines")
     } else {
         println("$fileName does not exist.")
     }
-
 }
