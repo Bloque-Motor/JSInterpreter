e8e2877f8d7e53a429f653c47fd5105c0e035a8b
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2018-10-22 21:50:31 +0200

    Removed useless variables

--- src/main.kt
+++ src/main.kt
@@ -8,16 +8,12 @@ fun main(args : Array<String>) {
     val file = File(fileName)
     println("Enter output filename:")
     val outFileName: String? = readLine()
+    var lines = 0
     if(file.exists()){
         println("$fileName does exist. Reading file...")
         println()
         val bufferedReader = file.bufferedReader()
         val text:List<String> = bufferedReader.readLines()
-        var caps = 0
-        var small = 0
-        var other = 0
-        var numbers = 0
-        var lines = 0
         for(line in text){
             println(line)
             var currentLine = line.toCharArray()
@@ -28,17 +24,6 @@ fun main(args : Array<String>) {
             lines++
         }
 
-        println()
-        println("___________________________________")
-        File(outFileName).printWriter().use { out ->
-            out.println("Chacacter count")
-        }
-        println()
-        println("Caps: $caps")
-        println("Small letters: $small")
-        println("Numbers: $numbers")
-        println("Other characters: $other")
-        println("""Total number of characters: ${small + caps + numbers + other}""")
         File(outFileName).printWriter().use { out ->
             out.println("Number of lines: $lines")
         }
