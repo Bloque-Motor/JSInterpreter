415329179f654b94044c2d768e2484596ab26b2e
Saabertooth1 <nalas3@icloud.com>
2020-01-18 17:26:16 +0100

    States implementation closes #2 #3 #4 #5 #6 #7 #8 #9 #10 #11 #12 #13

--- src/SyntaxAnalyzer.kt
+++ src/SyntaxAnalyzer.kt
@@ -1,6 +1,4 @@
-import java.lang.Exception
-
-class SyntaxAnalyzer (private val tokenStream: List<Token>) {
+class SyntaxAnalyzer(private val tokenStream: List<Token>) {
 
     enum class States {
         P, B, B1, T, S, S1, S2, S3, S4, X, C, F, H, A, K, L, Q, E, R, U, U1, U2, V, V1
@@ -9,45 +7,45 @@ class SyntaxAnalyzer (private val tokenStream: List<Token>) {
     private var stack: Stack = Stack()
     private var parseOrder = mutableListOf<Int>()
 
-    fun parse(){
+    fun parse() {
         var currentIndex = 0
         var currentToken: Token
         stack.push(States.P)
-        while(tokenStream.isNotEmpty()){
+        while (tokenStream.isNotEmpty()) {
             currentToken = tokenStream[currentIndex]
             if (stack.peek() is Token) {
                 if ((stack.peek() as Token).type == currentToken.type) {
                     stack.pop()
                     currentIndex++
-                }else{
+                } else {
                     throw Exception("Syntax Error on token $currentIndex. Expected \"${(stack.peek() as Token).type}\"")
                 }
-            }else{
-                when(stack.peek()){
+            } else {
+                when (stack.peek()) {
                     States.P -> stateP(currentToken)
-                    States.B-> stateB(currentToken)
-                    States.B1-> stateB1(currentToken)
-                    States.T-> stateT(currentToken)
-                    States.S-> stateS(currentToken)
-                    States.S1-> stateS1(currentToken)
-                    States.S2-> stateS2(currentToken)
-                    States.S3-> stateS3(currentToken)
-                    States.S4-> stateS4(currentToken)
-                    States.X-> stateX(currentToken)
-                    States.C-> stateC(currentToken)
-                    States.F-> stateF(currentToken)
-                    States.H-> stateH(currentToken)
-                    States.A-> stateA(currentToken)
-                    States.K-> stateK(currentToken)
-                    States.L-> stateL(currentToken)
-                    States.Q-> stateQ(currentToken)
-                    States.E-> stateE(currentToken)
-                    States.R-> stateR(currentToken)
-                    States.U-> stateU(currentToken)
-                    States.U1-> stateU1(currentToken)
-                    States.U2-> stateU2(currentToken)
-                    States.V-> stateV(currentToken)
-                    States.V1-> stateV1(currentToken)
+                    States.B -> stateB(currentToken)
+                    States.B1 -> stateB1(currentToken)
+                    States.T -> stateT(currentToken)
+                    States.S -> stateS(currentToken)
+                    States.S1 -> stateS1(currentToken)
+                    States.S2 -> stateS2(currentToken)
+                    States.S3 -> stateS3(currentToken)
+                    States.S4 -> stateS4(currentToken)
+                    States.X -> stateX(currentToken)
+                    States.C -> stateC(currentToken)
+                    States.F -> stateF(currentToken)
+                    States.H -> stateH(currentToken)
+                    States.A -> stateA(currentToken)
+                    States.K -> stateK(currentToken)
+                    States.L -> stateL(currentToken)
+                    States.Q -> stateQ(currentToken)
+                    States.E -> stateE(currentToken)
+                    States.R -> stateR(currentToken)
+                    States.U -> stateU(currentToken)
+                    States.U1 -> stateU1(currentToken)
+                    States.U2 -> stateU2(currentToken)
+                    States.V -> stateV(currentToken)
+                    States.V1 -> stateV1(currentToken)
                 }
             }
         }
@@ -55,7 +53,7 @@ class SyntaxAnalyzer (private val tokenStream: List<Token>) {
 
 
     private fun stateP(token: Token) {
-        when (token.type){
+        when (token.type) {
             "var", "if", "while", "id", "return", "print", "input" -> {
                 stack.pop()
                 stack.push(States.P)
@@ -84,10 +82,11 @@ class SyntaxAnalyzer (private val tokenStream: List<Token>) {
                 stack.push(Token(";", ""))
                 stack.push(States.S2)
                 stack.push(States.T)
-                stack.push(Token("var",""))
+                stack.push(Token("var", ""))
                 parseOrder.add(4)
             }
             "if" -> {
+                stack.pop()
                 stack.push(States.B1)
                 stack.push(Token(")", ""))
                 stack.push(States.E)
@@ -111,55 +110,244 @@ class SyntaxAnalyzer (private val tokenStream: List<Token>) {
                 stack.push(States.S)
                 parseOrder.add(7)
             }
+            else -> throw Exception("Syntax error. State B received ${token.type} token.")
         }
     }
 
     private fun stateB1(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "id", "return", "print", "input" -> {
+                stack.pop()
+                stack.push(States.S)
+                parseOrder.add(8)
+            }
+            "{" -> {
+                stack.pop()
+                stack.push(Token("}", ""))
+                stack.push(States.C)
+                stack.push(Token("{", ""))
+                parseOrder.add(9)
+            }
+            else -> throw Exception("Syntax error. State B1 received ${token.type} token.")
+        }
     }
 
     private fun stateT(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "int" -> {
+                stack.pop()
+                stack.push(Token("int", ""))
+                parseOrder.add(10)
+            }
+            "string" -> {
+                stack.pop()
+                stack.push(Token("string", ""))
+                parseOrder.add(11)
+            }
+            "boolean" -> {
+                stack.pop()
+                stack.push(Token("boolean", ""))
+            }
+            else -> throw Exception("Syntax error. State T  received ${token.type} token.")
+        }
     }
 
     private fun stateS(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "id" -> {
+                stack.pop()
+                stack.push(States.S1)
+                stack.push(Token("id", ""))
+                parseOrder.add(13)
+            }
+            "return" -> {
+                stack.pop()
+                stack.push(Token(";", ""))
+                stack.push(States.X)
+                stack.push(Token("return", ""))
+                parseOrder.add(14)
+            }
+            "print" -> {
+                stack.pop()
+                stack.push(Token(";", ""))
+                stack.push(Token(")", ""))
+                stack.push(States.L)
+                stack.push(Token("(", ""))
+                stack.push(Token("print", ""))
+                parseOrder.add(15)
+            }
+            "input" -> {
+                stack.pop()
+                stack.push(Token(";", ""))
+                stack.push(Token(")", ""))
+                stack.push(Token("id", ""))
+                stack.push(Token("(", ""))
+                stack.push(Token("input", ""))
+                parseOrder.add(16)
+            }
+            else -> throw Exception("Syntax error. State S received ${token.type} token.")
+        }
     }
 
     private fun stateS1(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "=" -> {
+                stack.pop()
+                stack.push(Token(";", ""))
+                stack.push(States.E)
+                stack.push(Token("=", ""))
+                parseOrder.add(17)
+            }
+            "-=" -> {
+                stack.pop()
+                stack.push(Token(";", ""))
+                stack.push(States.E)
+                stack.push(Token("-=", ""))
+                parseOrder.add(18)
+            }
+            "(" -> {
+                stack.pop()
+                stack.push(Token(";", ""))
+                stack.push(Token(")", ""))
+                stack.push(States.L)
+                stack.push(Token("(", ""))
+                parseOrder.add(19)
+            }
+            else -> throw Exception("Syntax error. State S1 received ${token.type} token.")
+        }
     }
 
     private fun stateS2(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "id" -> {
+                stack.pop()
+                stack.push(States.S3)
+                stack.push(Token("id", ""))
+                parseOrder.add(20)
+            }
+            else -> throw Exception("Syntax error. State S2 received ${token.type} token.")
+        }
     }
 
     private fun stateS3(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "=" -> {
+                stack.pop()
+                stack.push(States.S4)
+                stack.push(States.E)
+                stack.push(Token("=", ""))
+                parseOrder.add(21)
+            }
+            "," -> {
+                stack.pop()
+                stack.push(States.S2)
+                stack.push(Token(",", ""))
+                parseOrder.add(22)
+            }
+            ";" -> {
+                stack.pop()
+                parseOrder.add(23)
+            }
+            else -> throw Exception("Syntax error. State S3 received ${token.type} token.")
+        }
     }
 
     private fun stateS4(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "," -> {
+                stack.pop()
+                stack.push(States.S2)
+                stack.push(Token(",", ""))
+                parseOrder.add(24)
+            }
+            ";" -> {
+                stack.pop()
+                parseOrder.add(25)
+            }
+            else -> throw Exception("Syntax error. State S4 received ${token.type} token.")
+        }
     }
 
     private fun stateX(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "!", "id", "number", "(", "cadena", "false", "true" -> {
+                stack.pop()
+                stack.push(States.E)
+                parseOrder.add(26)
+            }
+            ";" -> {
+                stack.pop()
+                parseOrder.add(27)
+            }
+            else -> throw Exception("Syntax error. State X received ${token.type} token.")
+        }
     }
 
     private fun stateC(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "var", "if", "while", "id", "return", "print", "input" -> {
+                stack.pop()
+                stack.push(States.C)
+                stack.push(States.B)
+                parseOrder.add(28)
+            }
+            "}" -> {
+                stack.pop()
+                parseOrder.add(29)
+            }
+            else -> throw Exception("Syntax error. State C received ${token.type} token.")
+        }
     }
 
     private fun stateF(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "function" -> {
+                stack.pop()
+                stack.push(Token("}", ""))
+                stack.push(States.C)
+                stack.push(Token("{", ""))
+                stack.push(Token(")", ""))
+                stack.push(States.A)
+                stack.push(Token("(", ""))
+                stack.push(Token("id", ""))
+                stack.push(States.H)
+                stack.push(Token("function", ""))
+                parseOrder.add(30)
+            }
+            else -> throw Exception("Syntax error. State F received ${token.type} token.")
+        }
     }
 
     private fun stateH(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "int", "string", "boolean" -> {
+                stack.pop()
+                stack.push(States.T)
+                parseOrder.add(31)
+            }
+            "id" -> {
+                stack.pop()
+                parseOrder.add(32)
+            }
+            else -> throw Exception("Syntax error. State H received ${token.type} token.")
+        }
     }
 
     private fun stateA(token: Token) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (token.type) {
+            "int", "string", "boolean" -> {
+                stack.pop()
+                stack.push(States.K)
+                stack.push(Token("id", ""))
+                stack.push(States.T)
+                parseOrder.add(33)
+            }
+            ")" -> {
+                stack.pop()
+                parseOrder.add(34)
+            }
+            else -> throw Exception("Syntax error. State A received ${token.type} token.")
+        }
     }
 
     private fun stateK(token: Token) {
@@ -202,17 +390,18 @@ class SyntaxAnalyzer (private val tokenStream: List<Token>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private class Stack{
+    private class Stack {
         val elements: MutableList<Any> = mutableListOf()
 
         fun isEmpty() = elements.isEmpty()
         fun size() = elements.size
         fun push(item: Any) = elements.add(item)
-        fun pop() : Any? {
+        fun pop(): Any? {
             val item = elements.lastOrNull()
             if (!isEmpty()) elements.removeAt(elements.size - 1)
             return item
         }
-        fun peek() : Any? = elements.lastOrNull()
+
+        fun peek(): Any? = elements.lastOrNull()
     }
 }
