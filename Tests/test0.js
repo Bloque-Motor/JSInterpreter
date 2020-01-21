f4d389e4f44f565b9a8988271c2c77bc48258136
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2020-01-21 03:08:45 +0100

    Fixed tests

--- Tests/test0.js
+++
@@ -1,6 +0,0 @@
-var int a = 2, b = 3;
-function int suma(int i, int j) {
-    return i + j;
-}
-
-var int c = suma(a, b);
--- Tests/test2.js
+++ Tests/test2.js
@@ -1,11 +1,6 @@
-function int test1(int i, boolean ok){
-    var int num;
-    input(num);
-    while(i < 4){
-        num -= i;
-        if (num < 8) ok = true;
-    }
-    var string hola = 'Hola caracola';
-    print(hola);
-    return num;
+var int a = 2, b = 3;
+function int suma(int i, int j) {
+    return i + j;
 }
+
+var int c = suma(a, b);
