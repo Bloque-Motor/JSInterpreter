7140a17fd57bf26a6d2a0e8e93fe263e4b324037
inigoar <inigoarpalacios@yahoo.es>
2020-06-16 17:47:50 +0200

    new branch for TS modifications

--- src/FilePrinter.kt
+++ src/FilePrinter.kt
@@ -2,10 +2,10 @@ import java.io.File
 
 class FilePrinter {
 
-    private val tokenFileName = """Output\Tokens.txt"""
-    private val symbolTableFile = """Output\SymbolTable.txt"""
-    private val errorFile = """Output\Errors.txt"""
-    private val parseFileName = """Output\ParseFile.txt"""
+    private val tokenFileName = """/Users/inigoar/Downloads/PDL/token.txt"""
+    private val symbolTableFile = """/Users/inigoar/Downloads/PDL/tablaSimbolos.txt"""
+    private val errorFile = """/Users/inigoar/Downloads/PDL/errores.txt"""
+    private val parseFileName = """/Users/inigoar/Downloads/PDL/parser.txt"""
 
     var assignments = mutableListOf<String>()
     var delimiters = mutableListOf<String>()
